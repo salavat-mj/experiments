a1b5ea464bf0cbedeb322d67bd77c03062a2f5f2
salavat-mj <salavat-mj@users.noreply.github.com>
2020-02-10 01:00:05 +0300

    test

--- CycleGAN/test.py
+++ CycleGAN/test.py
@@ -30,9 +30,8 @@ import os
 from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
-from util.visualizer import save_images
-#from util import html
-
+#from util.visualizer import save_images
+from CycleGAN.util.util import tensor2im, save_image
 
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
@@ -42,20 +41,21 @@ if __name__ == '__main__':
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+
+    opt.name = 'style_ukiyoe_pretrained'
+    opt.dataroot = 'testB'
+
+    opt.model = 'test'
+    opt.no_dropout = True
+    opt.results_dir = opt.dataroot
+
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
-    # create a website
-    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
-    print('creating web directory', web_dir)
     data = next(iter(dataset))
     model.set_input(data)  # unpack data from data loader
     model.test()           # run inference
     visuals = model.get_current_visuals()  # get image results
-    img_path = model.get_image_paths()     # get image paths
-
-    label, im_data = next(visuals.items())
-    if label == 'real':
-        label, im_data = next(visuals.items())
-    im = util.tensor2im(im_data)
-    util.save_image(im, web_dir, aspect_ratio=aspect_ratio)
+    im = tensor2im(visuals['fake'])
+    save_path = os.path.join(opt.dataroot, 'fake.png')
+    save_image(im, save_path, aspect_ratio=1.0)#, aspect_ratio=1.0)
